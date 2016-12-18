a56768b888d81e90003eae22358100b78985854b
lhvu <lehung.vu@gmail.com>
2016-12-18 16:32:01 +0100

    Chapter 8 fragement UI

---
+++ app/src/main/java/org/lhvu/home/criminalintent/Crime.java
@@ -0,0 +1,47 @@
+package org.lhvu.home.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Hung on 18/12/2016.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
